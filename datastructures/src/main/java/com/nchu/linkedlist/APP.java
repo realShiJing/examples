a244805e81e4e636b296effa722ca20f4a6e4211
wayfo <realshijing@163.com>
2019-08-01 17:54:03 +0800

    单向链表（添加和遍历）

---
+++ datastructures/src/main/java/com/nchu/linkedlist/APP.java
@@ -0,0 +1,39 @@
+package com.nchu.linkedlist;
+
+import org.junit.Test;
+
+/**
+ * @Decription 链表测试
+ * @Author yangsj
+ * @Date 20190801 10:07
+ **/
+public class APP {
+
+    /**
+     * @Description 测试单链表
+     * @Author yangsj
+     * @Date 2019/8/1 10:12
+     **/
+    @Test
+    public void TestSigleLinkedList(){
+        SingleLinkedList list = new SingleLinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.show();
+        System.out.println("---------------");
+        SingleLinkedList listSort = new SingleLinkedList();
+        listSort.addSort(2);
+        listSort.addSort(1);
+        listSort.addSort(3);
+        listSort.addSort(1);
+        listSort.addSort(4);
+        listSort.addSort(5);
+        listSort.addSort(6);
+        listSort.show();
+
+    }
+
+}
---
+++ datastructures/src/main/java/com/nchu/linkedlist/SingleLinkedList.java
@@ -0,0 +1,81 @@
+package com.nchu.linkedlist;
+
+/**
+ * @Decription 单链表
+ * @Author yangsj
+ * @Date 20190801 9:17
+ **/
+public class SingleLinkedList {
+    //节点
+    class Node{
+        //节点数据
+        public  int data;
+        //下个节点的引用
+        public  Node next;
+
+        Node(int data){
+            this.data = data;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{ data=" + data + " }";
+        }
+    }
+    //头结点
+    public Node headNode = new Node(0);
+
+    //添加节点到单链表的尾部
+    public void add(int data){
+        Node node = new Node(data);
+        //辅助节点，用于保存单链表最后一个节点
+        Node temp = headNode;
+        while (true){
+            if(temp.next == null){
+                break;
+            }
+            temp = temp.next;
+        }
+        //添加节点到最后一个节点的下个节点
+       temp.next = node;
+    }
+
+    //遍历列表
+    public void show(){
+        Node temp = headNode;
+        if(temp.next == null){
+            System.out.println("链表为空！");
+            return;
+        }
+
+        while (true){
+            temp = temp.next;
+            if(temp == null){
+                break;
+            }
+            System.out.println(temp);
+        }
+    }
+
+    //有序添加
+    public void addSort(int data){
+        Node node = new Node(data);
+        Node temp = headNode;
+        while(true){
+            //如果临时节点的下个节点为空，代表已经遍历到了最后，都没有符合条件的
+            //这时直接将节点加到链表的最后
+            if (temp.next == null){
+                temp.next = node;
+                return;
+            }
+            int tempData = temp.data;
+            int nextdata = temp.next.data;
+            if(tempData <= data && data < nextdata){
+                break;
+            }
+            temp = temp.next;
+        }
+        node.next = temp.next;
+        temp.next = node;
+    }
+}
