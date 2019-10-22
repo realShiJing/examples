0efe060765fd3fa9cef734d936bc0b30391236db
wayfo <realshijing@163.com>
2019-10-22 09:29:51 +0800

    volatile关键字重排序测试

--- thread/src/main/java/com/nchu/test/VolatileTest.java
+++ thread/src/main/java/com/nchu/test/VolatileTest.java
@@ -1,5 +1,7 @@
 package com.nchu.test;
 
+import org.junit.Test;
+
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -40,4 +42,50 @@ public class VolatileTest {
         System.out.println(test.inc);
     }
 
+
+    int a = 1 ;
+    volatile boolean flag = false;
+    int ret = 0 ;
+    public void write(){
+        a = 2;      //1
+        flag = true;//2
+    }
+
+    public void multiply(){
+        if(flag){         //3
+            ret = a*a;    //4
+        }
+    }
+
+
+    /**
+     * @Description 有序性测试
+     * 经测试，该种场景下不存在1和2步骤重排序的问题
+     * @Author yangsj
+     * @Date 2019/10/18 13:56
+     **/
+    @Test
+    public void OrderlyTest(){
+        for (int i = 0; i < 100 ; i++) {
+            VolatileTest test = new VolatileTest();
+            CountDownLatch latch = new CountDownLatch(2);
+            //线程A执行对数据和flag状态的改变
+            new Thread(() -> {
+                test.write();
+                latch.countDown();
+            }).start();
+            //线程B判断flag状态，并处理数据
+            new Thread(() -> {
+                test.multiply();
+                latch.countDown();
+            }).start();
+            try {
+                //等待两个线程执行完毕
+                latch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(test.ret);
+        }
+    }
 }
