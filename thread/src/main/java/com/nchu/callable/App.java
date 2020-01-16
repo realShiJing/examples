8ce94fec266f0b437ee64fc07a92962c73b64bc7
wayfo <realshijing@163.com>
2020-01-16 15:16:05 +0800

    FutureTask 测试

--- thread/src/main/java/com/nchu/callable/App.java
+++ thread/src/main/java/com/nchu/callable/App.java
@@ -2,10 +2,7 @@ package com.nchu.callable;
 
 import org.junit.Test;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 /**
  * @Decription Callable 测试
@@ -22,5 +19,23 @@ public class App {
         // 注意调用get方法会阻塞当前线程，直到得到结果。
         // 所以实际编码中建议使用可以设置超时时间的重载get方法。
         System.out.println(future.get());
+    }
+
+
+
+    /**
+     * @Description  FutureTask 测试
+     * @Author yangsj
+     * @Date 2020/1/6 16:54
+     **/
+    @Test
+    public void test1() throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        Task task = new Task();
+        FutureTask<Integer> futureTask = new FutureTask<>(task);
+        executorService.submit(futureTask);
+        // 注意调用get方法会阻塞当前线程，直到得到结果。
+        // 所以实际编码中建议使用可以设置超时时间的重载get方法。
+        System.out.println(futureTask.get());
     }
 }
