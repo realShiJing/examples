02c81dcd9c682229afc6e4319eea721782e84cf5
realshijing@163.com <ysjdemima321>
2020-02-12 16:29:57 +0800

    volatile : 保证可见性，禁止指令重排，不保证原子性

--- thread/src/main/java/com/nchu/test/VolatileTest.java
+++ thread/src/main/java/com/nchu/test/VolatileTest.java
@@ -11,17 +11,45 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @Date 20191016 16:00
  **/
 public class VolatileTest {
-    public /*volatile int*/ AtomicInteger inc = new AtomicInteger(0);
+   // public  AtomicInteger inc = new AtomicInteger(0);
+
+    public /*volatile*/ int inc = 0 ;
 
     public void increase() {
-        // inc++;
-        inc.getAndIncrement();
+        // 多线程环境下无法保证 i++ 的原子性
+        inc++;
+        //inc.getAndIncrement();
     }
+    /**
+     * @Description  可见性测试
+     * @Author yangsj
+     * @Date `2020/2/10` 5:14 下午
+     **/
+    @Test
+    public void test(){
+        VolatileTest test = new VolatileTest();
+        new Thread(()->{
+            System.out.println(Thread.currentThread().getName()+":"+test.inc);
+            // 让子线程睡一秒很关键，此时主线程已经在循环判断，如果睡眠结束后，共享变量的值被修改，但是主线程还在循环，证明主线程对子线程的修改不可见
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            test.increase();
+            System.out.println(Thread.currentThread().getName()+":"+test.inc);
+        }).start();
 
+        while(test.inc==0){
+            //System.out.println("主线程不可见子线程对共享变量的修改！"+test.inc);
+        }
+        System.out.println("主线程可见子线程对共享变量的修改！"+test.inc);
 
+    }
     /**
      * @Description 测试结果，每次打印的的值都不一样，
      * 证明volatile修饰的变量在多线程的环境下无法保持原子性
+     * 解决方案：用原子类
      * @Author yangsj
      * @Date 2019/10/17 15:37
      **/
@@ -44,7 +72,7 @@ public class VolatileTest {
 
 
     int a = 1 ;
-    volatile boolean flag = false;
+    /*volatile*/ boolean flag = false;
     int ret = 0 ;
     public void write(){
         a = 2;      //1
@@ -60,32 +88,34 @@ public class VolatileTest {
 
     /**
      * @Description 有序性测试
-     * 经测试，该种场景下不存在1和2步骤重排序的问题
+     * 步骤1和步骤2 在变量 flag 没有 volatile 修饰的情况下有可能发生指令重排
      * @Author yangsj
      * @Date 2019/10/18 13:56
      **/
     @Test
     public void OrderlyTest(){
-        for (int i = 0; i < 100 ; i++) {
+        for (int i = 0; i < 1000000 ; i++) {
             VolatileTest test = new VolatileTest();
-            CountDownLatch latch = new CountDownLatch(2);
+            //CountDownLatch latch = new CountDownLatch(1);
             //线程A执行对数据和flag状态的改变
             new Thread(() -> {
                 test.write();
-                latch.countDown();
+                //latch.countDown();
             }).start();
             //线程B判断flag状态，并处理数据
             new Thread(() -> {
                 test.multiply();
-                latch.countDown();
+                //latch.countDown();
             }).start();
-            try {
+            /*try {
                 //等待两个线程执行完毕
-                latch.await();
+                //latch.await();
             } catch (InterruptedException e) {
                 e.printStackTrace();
+            }*/
+            if(test.ret == 1){
+                System.out.println(test.ret);
             }
-            System.out.println(test.ret);
         }
     }
 }
