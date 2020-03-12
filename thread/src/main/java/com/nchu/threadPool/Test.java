6c2dff1e0b63895d011fdefe7e715ed63298655f
wayfo <realshijing@163.com>
2020-03-12 11:04:30 +0800

    线程池的使用

--- thread/src/main/java/com/nchu/threadPool/Test.java
+++ thread/src/main/java/com/nchu/threadPool/Test.java
@@ -1,21 +1,29 @@
 package com.nchu.threadPool;
 
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class Test {
 
-    public static void main(String[] args) {
-        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(2);
 
+    /**
+     * @Description juc线程池使用
+     * @Author yangsj
+     * @Date 2020/3/12 10:55
+     **/
+    public static void main(String[] args) {
+      // ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(2);
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 8, 100, TimeUnit.HOURS, new ArrayBlockingQueue<>(2), Executors.defaultThreadFactory(), new ThreadPoolExecutor.CallerRunsPolicy());
         for(int i=0;i<15;i++){
             final int index = i;
             executor.execute(new Runnable() {
                 @Override
                 public void run() {
-                    System.out.println("正在执行task "+index);
+                    System.out.println(Thread.currentThread().getName()+"正在执行task "+index);
                     try {
-                        Thread.currentThread().sleep(0);
+                        Thread.currentThread().sleep(1000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
