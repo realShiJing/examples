d1887c664258542686a1ac185307fa0fed349a96
realshijing@163.com <ysjdemima321>
2020-03-26 23:08:10 +0800

    线程中断相关测试

--- thread/src/main/java/com/nchu/interrupt/InterruptTest.java
+++ thread/src/main/java/com/nchu/interrupt/InterruptTest.java
@@ -144,4 +144,33 @@ public class InterruptTest {
         System.out.println(thread.isInterrupted());
         System.out.println(thread.getState());
     }
+
+    /**
+     * @Description interrupted()测试的是当前的线程的中断状态。
+     * 而isInterrupted()测试的是调用该方法的对象所表示的线程。
+     * 一个是静态方法（它测试的是当前线程的中断状态），
+     * 一个是实例方法（它测试的是实例对象所表示的线程的中断状态）。
+     * 一个会清除中断标识位，另一个不会清除中断标识位。
+     *
+     * @Author yangsj
+     * @Date 2020/1/8 14:26
+     **/
+    @Test
+    public void testInterrupted() throws InterruptedException {
+        Thread thread = new Thread(()->{
+            synchronized (this){
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    // 在该线程处于 TIMED_WAITING 状态时，中断该线程，会抛出 InterruptedException异常，该线程的中断标志位被清空
+                    System.out.println("catch InterruptedException");
+                }
+            }
+        });
+        // 开启线程
+        thread.start();
+        thread.interrupt();
+        System.out.println(thread.isInterrupted());
+        System.out.println(Thread.interrupted());
+    }
 }
