bc4413ed5725321e9819ae7fc084cb2f52e351ea
wayfo <realshijing@163.com>
2020-05-13 20:04:08 +0800

    Feign 初版

---
+++ cloud/eureka/feign/src/main/java/com/nchu/cloud/eureka/config/FeignConfig.java
@@ -0,0 +1,19 @@
+package com.nchu.cloud.eureka.config;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Decription OpenFeignClient配置
+ * @Author yangsj
+ * @Date 2020/5/13 11:37
+ **/
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    public Logger.Level feignLoggerLevel(){
+        return Logger.Level.FULL;
+    }
+}
