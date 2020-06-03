4f05792f44177e8bbe8c58b44cba53aa700abc8c
wayfo <realshijing@163.com>
2020-06-03 19:59:08 +0800

    Rocketmq 一个线程消费顺序消息

--- mq/rocketmq/src/main/java/com/example/order/OrderedConsumer.java
+++ mq/rocketmq/src/main/java/com/example/order/OrderedConsumer.java
@@ -18,6 +18,12 @@ public class OrderedConsumer {
 
         consumer.subscribe("orderTopic","*");
 
+        // MessageListenerOrderly 顺序消费 ,对一个 queue 开启一个线程，多个 queue 开多个线程
+        // 最大消费线程数
+        consumer.setConsumeThreadMax(1);
+        // 最小消费线程数
+        consumer.setConsumeThreadMin(1);
+        // 最大消费线程数和最小消费线程数均为1时，表示只允许一个线程消费
         consumer.registerMessageListener((MessageListenerOrderly) (msgs, context) -> {
             for (MessageExt msg : msgs) {
                 System.out.println(new String(msg.getBody())+"Thread:"+Thread.currentThread().getName()+"queueId"+msg.getQueueId());
